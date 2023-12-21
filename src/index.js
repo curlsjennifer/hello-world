6677c830d47fec59a87ae4ae3f9984f1414d9a14
WANYUNYANG <curlsjennifer@gmail.com>
2023-12-21 13:26:55 +0800

    modify index.js to remove useless function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import React, {Component, Fragment} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var a = "15";
-
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      <input type="date" id="datea"></input>
-      <h3>{a}</h3>
-      <li>{a}</li>
-    </div>
-    
-  );
-  root.render(element, document.getElementById('root'));
-
-}
-//setInterval(tick, 1000);
 root.render(<App />);
 reportWebVitals();
 
